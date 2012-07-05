967147de3edc5d18e19cb2498872327e01f4f3b0
Lode Hoste <zillode@zillode.be>
2012-07-05 02:21:12 +0200

    Added publish (.Net click-once) information

--- SkeletalTracking/SkeletalTracking/Properties/AssemblyInfo.cs
+++ SkeletalTracking/SkeletalTracking/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkeletalTracking")]
+[assembly: AssemblyTitle("OSCeleton-KinectSDK")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("SkeletalTracking")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCompany("Lode Hoste")]
+[assembly: AssemblyProduct("OSCeleton-KinectSDK")]
+[assembly: AssemblyCopyright("Copyright © Lode Hoste 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
